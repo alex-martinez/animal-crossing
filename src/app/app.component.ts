3abadf441c1d1c369d1a7542c5e1ff94ba6ea9a5
Alex Martinez <alexm@aceable.com>
2020-04-17 19:12:58 -0500

    Add search functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { INSECTS } from './data/insects';
 import { FISHES } from './data/fishes';
 import { Fish } from './interfaces/fish.interface';
 import { Insect } from './interfaces/insect.interface';
+import { fromEvent, Observable, of, concat, from } from 'rxjs';
+import { tap, map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  INSECTS: Insect[] = INSECTS;
-  FISHES: Fish[] = FISHES;
+export class AppComponent implements OnInit {
+  insects: Insect[] = INSECTS;
+  fishes: Fish[] = FISHES;
+  insects$: Observable<Insect[]>;
+
+  @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
 
   getMonthNames(monthList: number[]): string[] {
     if (monthList.length === 12) {
@@ -24,5 +29,30 @@ export class AppComponent {
     ];
 
     return monthList.map(m => months[m]);
+  }
+
+  ngOnInit() {
+    const searchText$ = fromEvent<any>(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+      map(event => event.target.value),
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(search => this.filterInsects(search))
+      );
+
+    this.insects$ = concat(of(this.insects), searchText$);
+  }
+
+  filterInsects(search = '') {
+    // const exactMatch = (insect) => RegExp(search.toLowerCase(), 'g').test(insect.name.toLowerCase());
+    const fuzzyMatch = (insect: Insect) => this.fuzzyMatch(search.toLowerCase(), insect.name.toLowerCase());
+    const filteredList = this.insects.filter(fuzzyMatch);
+    return of(filteredList);
+  }
+
+  fuzzyMatch(pattern: string, str: string) {
+    pattern = '.*' + pattern.split('').join('.*') + '.*';
+    const re = new RegExp(pattern);
+    return re.test(str);
   }
 }
---
+++ src/app/data/nook-miles-tasks.ts
@@ -0,0 +1,98 @@
+export const NOOK_MILES_TASKS = [
+  {
+    task: 'Catch five fish',
+    reward: '150 Nook Miles'
+  },
+  {
+    task: 'Catch five bugs',
+    reward: '150 Nook Miles'
+  },
+  {
+    task: 'Water eight flowers',
+    reward: '100 Nook Miles'
+  },
+  {
+    task: 'Plant three flowers',
+    reward: '100 Nook Miles'
+  },
+  {
+    task: 'Hit a rock five times',
+    reward: '100 Nook Miles'
+  },
+  {
+    task: 'Shoot down a balloon',
+    reward: '200 Nook Miles'
+  },
+  {
+    task: 'Plant one sapling',
+    reward: '100 Nook Miles'
+  },
+  {
+    task: 'Plant one fruit tree',
+    reward: '100 Nook Miles'
+  },
+  {
+    task: 'Talk to three villagers',
+    reward: '200 Nook Miles'
+  },
+  {
+    task: 'Sell items for 5,000 Bells',
+    reward: '150 Nook Miles'
+  },
+  {
+    task: 'Sell five fruit',
+    reward: '100 Nook Miles'
+  },
+  {
+    task: 'Make three DIY items',
+    reward: '150 Nook Miles'
+  },
+  {
+    task: 'Chop down a tree',
+    reward: '200 Nook Miles'
+  },
+  {
+    task: 'Chop 10 wood',
+    reward: '150 Nook Mile\'s'
+  },
+  {
+    task: 'Change your outfit using your closet',
+    reward: '150 Nook Miles'
+  },
+  {
+    task: 'Take a photo',
+    reward: '100 Nook Mile\'s'
+  },
+  {
+    task: 'Customize two items',
+    reward: '150 Nook Miles'
+  },
+  {
+    task: 'Sell a hot item',
+    reward: '200 Nook Miles'
+  },
+  {
+    task: 'Catch a specific bug',
+    reward: '200-400 Nook Miles'
+  },
+  {
+    task: 'Catch a specific fish',
+    reward: '200-400 Nook Miles'
+  },
+  {
+    task: 'Get three fossils assessed',
+    reward: '150 Nook Miles'
+  },
+  {
+    task: 'Spend 500 Nook Nook Miles',
+    reward: '200 Nook Miles'
+  },
+  {
+    task: 'Sell 10 shells',
+    reward: '200 Nook Miles'
+  },
+  {
+    task: 'Spend 5,000 Bells on buying items',
+    reward: '200 Nook Miles'
+  }
+];
